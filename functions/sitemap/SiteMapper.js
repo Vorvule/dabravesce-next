07d10f936e58091976309743f8d1628b09a37b71
Alexander Vorvule <7105690@gmail.com>
2024-06-29 23:54:55 +0300

    SiteMap.xml and Manifest.json added

---
+++ functions/sitemap/SiteMapper.js
@@ -0,0 +1,34 @@
+import AppSources from "@/assets/albums/AppSources";
+
+const createSitemap = () => {
+  let urlSet = "";
+
+  let path = "";
+  urlSet += getUrlLoc(path);
+
+  path = "/sources";
+  urlSet += getUrlLoc(path);
+
+  AppSources.map((album) => {
+    album.text.map((book) => {
+      book.text.map((chapter) => {
+        path = "/content/" + [album.slug, book.slug, chapter.slug].join("~");
+        urlSet += getUrlLoc(path);
+      });
+    });
+  });
+
+  const sitemap =
+    '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n' +
+    urlSet +
+    "</urlset>";
+
+  console.log(sitemap);
+};
+
+const getUrlLoc = (path) => {
+  return `<url><loc>https://dabravesce.by${path}</loc></url>\n`;
+};
+
+export default createSitemap;
--- readme-update.md
+++ readme-update.md
@@ -3,7 +3,41 @@
 ## To update Expo SDK
 
 - Install the latest Expo SDK version:
-  > npm install expo@latest
+
+> npm install expo@latest
 
 - Upgrade all dependencies to match the installed SDK version:
-  > npx expo install --fix
+
+> npx expo install --fix
+
+- To review and upgrade your dependencies use
+
+> npx expo install --check
+
+## Expo Doctor
+
+- Make sure the code is valid:
+
+> npx -y expo-doctor
+
+## Build
+
+- Android:
+
+> eas build --platform android
+
+## Run
+
+- Web:
+
+> npm run web
+
+## To do
+
+- Settings
+
+  - Font size
+  - Initial page
+
+- Евангелле дня:
+  - От Матфея: 19:4,5; 19:9; :13,14; :16,17; :23,24; :28,30; 7:1,2
