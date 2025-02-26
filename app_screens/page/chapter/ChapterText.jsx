a70846fd737168b103905c67114cdeb603c62f6e
Alexander Vorvul <7105690@gmail.com>
2025-02-27 01:05:01 +0300

    Chapter text enhanced

--- app_screens/page/chapter/ChapterText.jsx
+++ app_screens/page/chapter/ChapterText.jsx
@@ -1,9 +1,23 @@
+import { Link } from 'expo-router';
+
 import ThemedText from '@/components/ThemedText';
+
+import { ColorTheme } from '@/functions/ColorTheme';
 
 export default function ChapterText({ chapterText }) {
   const tab = '       ';
+  const linkTextStyle = {
+    color: ColorTheme.getColor('link'),
+    fontFamily: 'Vollkorn',
+  };
 
   return chapterText.map((paragraph, index) => {
-    return <ThemedText key={'para-' + index}>{tab + paragraph}</ThemedText>;
+    return typeof paragraph === 'string' ? (
+      <ThemedText key={'para-' + index}>{tab + paragraph}</ThemedText>
+    ) : (
+      <Link href={paragraph[1]}>
+        <ThemedText style={linkTextStyle}>{tab + paragraph[0]}</ThemedText>
+      </Link>
+    );
   });
 }
