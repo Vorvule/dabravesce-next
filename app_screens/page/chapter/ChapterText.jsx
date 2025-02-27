9a69aa426a2c7ecc7aa3e328c8db563274558119
Alexander Vorvul <7105690@gmail.com>
2025-02-27 16:55:26 +0300

    Sitemap.xml updated

--- app_screens/page/chapter/ChapterText.jsx
+++ app_screens/page/chapter/ChapterText.jsx
@@ -13,7 +13,7 @@ export default function ChapterText({ chapterText }) {
 
   return chapterText.map((paragraph, index) => {
     return typeof paragraph === 'string' ? (
-      <ThemedText key={'para-' + index}>{tab + paragraph}</ThemedText>
+      <ThemedText key={'p-' + index}>{tab + paragraph}</ThemedText>
     ) : (
       <Link href={paragraph[1]}>
         <ThemedText style={linkTextStyle}>{tab + paragraph[0]}</ThemedText>
