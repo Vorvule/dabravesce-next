d413817a67ecc9517871fc16d2519c5cf1769071
Alexander Vorvul <7105690@gmail.com>
2025-12-16 22:17:22 +0300

    Button states singled out

--- app_screens/page/chapter/ChapterAudio.jsx
+++ app_screens/page/chapter/ChapterAudio.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { View } from 'react-native';
 
 import { useAudioPlayer, useAudioPlayerStatus } from 'expo-audio';
@@ -9,87 +9,70 @@ import { createClient } from '@supabase/supabase-js';
 import Device from '@/functions/Device';
 import RoundButton from '@/components/RoundButton';
 import Styles from '@/constants/styles/common.styles';
+import { BUTTON_STATE } from '@/constants/audio.button.states';
 
 export default function ChapterAudio({ chapterAudio }) {
-  const platformIsNative = !Device.platformIsWeb();
-
-  const stoppedState = { play: true, pause: false, stop: false };
-  const playingState = { play: false, pause: true, stop: true };
-  const pausedState = { play: true, pause: false, stop: true };
-
-  const [enabledButtons, setEnabledButtons] = useState(stoppedState);
-
   const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
-
   const supabase = createClient(supabaseUrl, supabaseKey);
 
   const [audioSource, setAudioSource] = useState(null);
+  const [buttons, setButtons] = useState(BUTTON_STATE.STOPPED);
 
   const player = useAudioPlayer(audioSource);
-   // console.log('Audio source', audioSource);
-   // console.log('Player duration', player.duration);
-   // console.log('Player current time', player.currentTime);
-   // console.log('Player buffering', player.isBuffering);
-   // console.log('Player loaded', player.isLoaded);
-   // console.log('Player paused', player.paused);
-   // console.log('Player rate', player.playbackRate);
-   // console.log('Player playing', player.playing);
-   // console.log('Player volume', player.volume);
-
-  // const handlePlaybackStatus = useCallback((status) => {
-  //   if (status.didJustFinish) {
-  //     console.log('Аудио завершилось');
-  //     setEnabledButtons(stoppedState);
-  //   }
-  // });
-
-  // const status = useAudioPlayerStatus(player);
-  // handlePlaybackStatus(status);
+  // console.log('Audio source', audioSource);
+  // console.log('Player duration', player.duration);
+  // console.log('Player current time', player.currentTime);
+  // console.log('Player buffering', player.isBuffering);
+  // console.log('Player loaded', player.isLoaded);
+  // console.log('Player paused', player.paused);
+  // console.log('Player rate', player.playbackRate);
+  // console.log('Player playing', player.playing);
+  // console.log('Player volume', player.volume);
 
   useEffect(() => {
     const { data } = supabase.storage.from('audio').getPublicUrl(chapterAudio);
     setAudioSource(data.publicUrl);
   }, [chapterAudio]);
 
-  const playAudio = async () => {
-    player.play();
-    setEnabledButtons(playingState);
+  const status = useAudioPlayerStatus(player);
 
-    player.playbackRate = 2;
-    platformIsNative && (await activateKeepAwakeAsync());
+  useMemo(() => {
+    status.didJustFinish && setButtons(BUTTON_STATE.STOPPED);
+  }, [status.didJustFinish]);
+
+  const platformIsNative = !Device.platformIsWeb();
+
+  const playAudio = async () => {
+    if (player.paused) {
+      player.play();
+      setButtons(BUTTON_STATE.PLAYING);
+      platformIsNative && (await activateKeepAwakeAsync());
+    }
   };
 
   const pauseAudio = async () => {
-    player.pause();
-    setEnabledButtons(pausedState);
-    platformIsNative && (await deactivateKeepAwake());
+    if (status.playing) {
+      player.pause();
+      setButtons(BUTTON_STATE.PAUSED);
+      platformIsNative && (await deactivateKeepAwake());
+    }
   };
 
   const stopAudio = async () => {
-    player.pause();
-    await player.seekTo(0);
-    setEnabledButtons(stoppedState);
-    platformIsNative && (await deactivateKeepAwake());
+    if (player.currentTime > 0) {
+      player.pause();
+      await player.seekTo(0);
+      setButtons(BUTTON_STATE.STOPPED);
+      platformIsNative && (await deactivateKeepAwake());
+    }
   };
 
   return (
     <View style={Styles.buttons}>
-      <RoundButton
-        name='play'
-        onPress={playAudio}
-        enabled={enabledButtons.play}
-      />
-      <RoundButton
-        name='pause'
-        onPress={pauseAudio}
-        enabled={enabledButtons.pause}
-      />
-      <RoundButton
-        name='stop'
-        onPress={stopAudio}
-        enabled={enabledButtons.stop}
-      />
+      <RoundButton name='play' onPress={playAudio} enabled={buttons.play} />
+      <RoundButton name='pause' onPress={pauseAudio} enabled={buttons.pause} />
+      <RoundButton name='stop' onPress={stopAudio} enabled={buttons.stop} />
     </View>
   );
 }
---
+++ constants/audio.button.states.js
@@ -0,0 +1,5 @@
+export const BUTTON_STATE = {
+  STOPPED: { play: true, pause: false, stop: false },
+  PLAYING: { play: false, pause: true, stop: true },
+  PAUSED: { play: true, pause: false, stop: true },
+};
