39bb277d7e3aee0ea16928e0b13ed9ab7d865137
Alexander Vorvul <7105690@gmail.com>
2026-06-06 14:34:01 +0300

    Device.getWindowWidth() refactored

--- functions/Device.ts
+++ functions/Device.ts
@@ -2,13 +2,12 @@ import { Dimensions, Platform } from 'react-native';
 
 export default class Device {
   static windowIsWide(): boolean {
-    if (Platform.OS === 'web' && typeof window !== 'undefined') {
-      return window.innerWidth > 800;
-    }
     return this.getWindowWidth() > 800;
   }
 
   static getWindowWidth(): number {
-    return Dimensions.get('window').width;
+    return (Platform.OS === 'web' && typeof window !== 'undefined')
+      ? window.innerWidth
+      : Dimensions.get('window').width;
   }
 }
