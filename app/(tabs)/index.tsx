79ef75a4bde0de8917795d8d7f1c8d97e1727cfe
Alexander Vorvul <7105690@gmail.com>
2026-06-15 22:00:44 +0300

    The index.tsx cleared out and enhanced with a daily Gospel hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,14 @@
-import { useContext, useState } from 'react';
-import { Platform, useWindowDimensions } from 'react-native';
-import { Redirect, usePathname } from 'expo-router';
-import Head from 'expo-router/head';
+import { useWindowDimensions } from 'react-native';
+import { Redirect } from 'expo-router';
 
-import Web from '@/services/web';
-import { GlobalContext } from '@/contexts/global.context';
-import { calendarDates } from '@/applets/calendar/model/calendar.dates';
-import { eventDates } from '@/applets/calendar/logic/event.dates';
-import PageScrollView from '../../components/page/page.scroll.view';
-import CalendarView from '../../applets/calendar/view/calendar.view';
 import { VERY_WIDE } from '@/constants/breakpoints';
-import Page from '../../services/page';
+import useDailyGospelUrl from '../../hooks/use.daily.gospel.url';
 
 export default function CalendarScreen() {
-  const path: string = usePathname();
   const { width } = useWindowDimensions();
-  const { dailyKeychain } = useContext(GlobalContext);
-  const [selectedDate, setSelectedDate] = useState<string>(calendarDates.getISODate());
-  const dayMonth = eventDates.getSelectedDayAndMonth(selectedDate);
+  const dailyGospelUrl = useDailyGospelUrl();
 
-  if (width > VERY_WIDE) {
-    return <Redirect href={Page.getUrl(dailyKeychain)} />;
-  } else {
-    return <Redirect href="/calendar" />;
-  }
+  const href = width > VERY_WIDE ? dailyGospelUrl : '/calendar';
+
+  return <Redirect href={href} />;
 }
