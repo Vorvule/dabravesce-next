5a1c2d06ed0ec7fa8f4786b1ff3a0327b81c749f
Alexander Vorvul <7105690@gmail.com>
2026-06-16 21:23:21 +0300

    Menu footer content enhanced

--- applets/menu/menu.footer.tsx
+++ applets/menu/menu.footer.tsx
@@ -1,15 +1,13 @@
-import ThemedText from '@/components/themed/themed.text';
+import ThemedText from '../../components/themed/themed.text';
 
 export default function MenuFooter() {
   const style: any = { textAlign: 'center', paddingVertical: 40 };
 
-  return (
-    <ThemedText type="item" style={style}>
-      Праект{'\n'}
-      Брацтва Віленскіх мучанікаў{'\n'}
-      Свята-Петра-Паўлаўскага сабора{'\n'}
-      Беларускай Праваслаўнай{'\n'}
-      Царквы
-    </ThemedText>
-  );
+  const footer = `Праект
+Брацтва Віленскіх мучанікаў
+Свята-Петра-Паўлаўскага сабора
+Беларускай Праваслаўнай
+Царквы`;
+
+  return <ThemedText type="item" style={style}>{ footer }</ThemedText>;
 }
