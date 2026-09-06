ad4e51f1748a6a455b93bbb49d639e1151ff4789
Alexander Vorvul <7105690@gmail.com>
2026-09-06 15:38:31 +0300

    YT button added

--- applets/menu/menu.footer.tsx
+++ applets/menu/menu.footer.tsx
@@ -1,7 +1,12 @@
+import { Pressable, StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useThemeColor } from '../../hooks/use.theme.color';
 import ThemedText from '../../components/themed/themed.text';
+import { ExternalLink } from '../../components/external.link';
 
 export default function MenuFooter() {
   const style: any = { textAlign: 'center', paddingVertical: 40 };
+  const linkColor = useThemeColor({}, 'link');
 
   const footer = `Праект
 Брацтва Віленскіх мучанікаў
@@ -9,5 +14,26 @@ export default function MenuFooter() {
 Беларускай Праваслаўнай
 Царквы`;
 
-  return <ThemedText type="item" style={style}>{ footer }</ThemedText>;
+  return (
+    <View>
+      <ThemedText type="item" style={style}>{ footer }</ThemedText>
+      <ExternalLink href="https://www.youtube.com/@Dabravesce" style={styles.link}>
+        <Pressable style={styles.button}>
+          <Ionicons name="logo-youtube" size={28} color="#FF0000" />
+          <ThemedText type="link" style={{ color: linkColor }}>Наш канал</ThemedText>
+        </Pressable>
+      </ExternalLink>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  link: {
+    alignSelf: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+});
