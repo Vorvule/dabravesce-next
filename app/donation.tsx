8942e8ebc0e210ed7f3a677f0a4efe638ff8ccbc
Alexander Vorvul <7105690@gmail.com>
2026-09-16 15:52:25 +0300

    Add content to donation.ts

--- app/donation.tsx
+++ app/donation.tsx
@@ -3,9 +3,21 @@ import Head from 'expo-router/head';
 
 import Web from '@/services/web';
 import ColumnLayout from '@/applets/layout/column.layout';
+import ThemedView from '@/components/themed/themed.view';
+import Styles from '@/constants/styles/common.styles';
 import ThemedText from '../components/themed/themed.text';
+import { LAYOUT } from '../constants/styles/layout';
 
 export default function DonationScreen() {
+  const paragraph1 = 'Пры мажлівасці і жаданні падтрымаць дзейнасць Брацтва ' +
+    'ў гонар святых Віленскіх мучанікаў Антонія, Іаана і Яўстафія вы можаце здзейсніць ' +
+    'ахвяраванне паводле наступных рэквізітаў:';
+
+  const paragraph2 = 'Без камісіі плацёж можна здзейсніць у ОАО «АСБ Беларусбанк». ' +
+    'Іншыя банкі могуць узяць за перавод нязначную камісію.';
+
+  const paragraph3 = 'Шчыра будзем удзячны вам і за найменшую лепту.';
+
   return (
     <>
       {Platform.OS === 'web' && (
@@ -15,7 +27,20 @@ export default function DonationScreen() {
         </Head>
       )}
 
-      <ColumnLayout title="Ахвяраванне" subtitle="">
+      <ColumnLayout title="Ахвяраванне" subtitle="На дзейнасць Брацтва">
+        <ThemedView style={Styles.paragraph}>
+          <ThemedText>{ LAYOUT.TAB + paragraph1 }</ThemedText>
+          <ThemedText> ●  УНП: 100 184 122</ThemedText>
+          <ThemedText> ●  Нумар рахунка ў фармаце IBAN: BY08 AKBB 3015 0000 0232 1000 0000</ThemedText>
+          <ThemedText> ●  Банк-атрымальнік: ОАО «АСБ Беларусбанк»</ThemedText>
+          <ThemedText> ●  Код банка (БІК): AKBBBY2X</ThemedText>
+          <ThemedText> ●  Лічбавы код банка: 795</ThemedText>
+          <ThemedText> ●  Атрымальнік: Праваслаўнае Брацтва ў гонар Віленскіх мучанікаў Антонія, Іаана і Яўстафія ў г. Мінску</ThemedText>
+          <ThemedText> ●  Код плацяжу: 44201 (альбо 144201) — Бязвыплатная (спонсарская) дапамога, дабравольныя ўзносы</ThemedText>
+          <ThemedText> ●  Прызначэнне плацяжу: Дабравольныя ахвяраванні</ThemedText>
+          <ThemedText>{ LAYOUT.TAB + paragraph2 }</ThemedText>
+          <ThemedText>{ LAYOUT.TAB + paragraph3 }</ThemedText>
+        </ThemedView>
       </ColumnLayout>
     </>
   );
--- applets/page/chapter/chapter.text.jsx
+++ applets/page/chapter/chapter.text.jsx
@@ -3,10 +3,9 @@ import { Link } from 'expo-router';
 import ThemedText from '@/components/themed/themed.text';
 
 import { useThemeColor } from '@/hooks/use.theme.color';
+import { LAYOUT } from '../../../constants/styles/layout.ts';
 
 export default function ChapterText({ chapterText }) {
-  const tab = '       ';
-
   const linkTextStyle = {
     color: useThemeColor({}, 'link'),
     fontFamily: 'Vollkorn',
@@ -15,10 +14,10 @@ export default function ChapterText({ chapterText }) {
   return chapterText.map((paragraph, index) => {
     // Todo: Move links (about Dabravesce) to the footer and simplify the code below
     return typeof paragraph === 'string' ? (
-      <ThemedText key={'p-' + index}>{tab + paragraph}</ThemedText>
+      <ThemedText key={'p-' + index}>{LAYOUT.TAB + paragraph}</ThemedText>
     ) : (
       <Link key={'l-' + index} href={paragraph[1]}>
-        <ThemedText style={linkTextStyle}>{tab + paragraph[0]}</ThemedText>
+        <ThemedText style={linkTextStyle}>{LAYOUT.TAB + paragraph[0]}</ThemedText>
       </Link>
     );
   });
